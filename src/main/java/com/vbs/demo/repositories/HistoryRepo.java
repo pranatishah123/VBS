1d9490c00454c0c22e28139938aa33e7ff585c6a
Bharat S Shah <bharat230268@outlook.com>
2025-12-31 08:08:08 +0530

    second commit

--- src/main/java/com/vbs/demo/repositories/HistoryRepo.java
+++ src/main/java/com/vbs/demo/repositories/HistoryRepo.java
@@ -4,6 +4,9 @@ import com.vbs.demo.models.History;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HistoryRepo extends JpaRepository<History,Integer> {
+
 }
